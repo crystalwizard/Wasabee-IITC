0b5c55c852468368ebc47c661327b4324fb95eab
Scot Bontrager <scot@indievisible.org>
2021-05-16 20:35:39 -0500

    enough for tonight

--- src/code/mapDrawing.js
+++ src/code/mapDrawing.js
@@ -14,6 +14,8 @@ export function drawMap() {
   updateAnchors(operation);
   updateMarkers(operation);
   resetLinks(operation);
+  // XXX do not do this here, make a special hook for it, this won't change often
+  resetZones(operation);
 }
 
 function updateMarkers(op) {
@@ -134,6 +136,14 @@ export function drawBackgroundOp(operation, layerGroup, style) {
 
     const newlink = new L.GeodesicPolyline(latLngs, style);
     newlink.addTo(layerGroup);
+  }
+}
+
+function resetZones(op) {
+  // remove all zones
+  for (const z of op.zones) {
+    console.log(z);
+    // create a new polygon, defined by z.points, in order
   }
 }
 
